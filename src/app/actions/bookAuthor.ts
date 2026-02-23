11f4a51d5b2b4c8f550ad8a2991217caae76ca41
Alexandrine Dubé <alexandrine.dube@hotmail.com>
2026-02-23 03:00:52 -0500

    fix: add prisma import comme il faut

--- src/app/actions/bookAuthor.ts
+++ src/app/actions/bookAuthor.ts
@@ -1,6 +1,6 @@
 
 "use server";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 export async function createBookAuthor({ idBook, idAuthor }: { idBook: number, idAuthor: number }) {
   try {
